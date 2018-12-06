5491f0043c0f5a9f642575007670678e13b7433b
kevinmartinez <7007533}+{kevinmartinez}@users.noreply.github.com>
2018-12-06 15:30:39 +0100

    add selectors and test input event handling WIP

--- js/app.js
+++ js/app.js
@@ -2,3 +2,25 @@
 console.log('start program')
 
 // Image search
+
+// Input field
+const inputSearch = document.querySelector('.input__search')
+console.log(inputSearch)
+
+inputSearch.addEventListener('input', (e) => {
+  console.log(inputSearch.value)
+})
+
+// Images
+const captions = document.querySelectorAll('figcaption')
+
+for (let i = 0; i <= captions.length; i += 1) {
+  console.log(captions[i])
+}
+
+// Searchfilter
+const buttonSearch = document.querySelector('.button__search')
+
+buttonSearch.addEventListener('click', (e) => {
+
+})
