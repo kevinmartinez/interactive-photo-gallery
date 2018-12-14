02ab39a33cb599cc39d0710599cc318e3edb8593
kevinmartinez <7007533}+{kevinmartinez}@users.noreply.github.com>
2018-12-14 05:43:44 +0100

    test media query, format code§ with JShint

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
 // My js code
-console.log('start program')
+console.log('start program');
 
 // Image search
 
 // Input field
-const inputSearch = document.querySelector('.input__search')
-console.log(inputSearch)
+const inputSearch = document.querySelector('.input__search');
+console.log(inputSearch);
 
 // Filter images
 inputSearch.addEventListener('input', (e) => {
-  console.log(e)
+  console.log(e);
   for (let i = 0; i < captions.length; i += 1) {
     if (captions[i].textContent.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())) {
-      console.dir(captions[i])
-      console.log(captions[i].textContent)
-      captions[i].parentElement.style.display = 'block'
+      console.dir(captions[i]);
+      console.log(captions[i].textContent);
+      captions[i].parentElement.style.display = 'block';
     } else {
-      captions[i].parentElement.style.display = 'none'
+      captions[i].parentElement.style.display = 'none';
     }
   }
 
-})
+});
 
 // Images
-const captions = document.querySelectorAll('figcaption')
+const captions = document.querySelectorAll('figcaption');
 
 // Searchfilter
-const buttonSearch = document.querySelector('.button__search')
+const buttonSearch = document.querySelector('.button__search');
