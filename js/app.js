058dee36f7950168d20890b45535ff4888548698
kevinmartinez <7007533}+{kevinmartinez}@users.noreply.github.com>
2019-10-06 14:09:06 +0200

    refactor code

--- js/app.js
+++ js/app.js
@@ -1,26 +1,21 @@
-// My js code
-
-// TODO: Remove in production
-console.log('start program');
-
-// TODO: Remove test code after optomization/refactoring
-// console.log(captions)
-
-// captions.forEach((element, x, y) => {
-//   console.log(element)
-//   console.log('x: ', x)
-//   console.log('y: ', y)
-// });
-
 // We need the text in the search input
 const inputSearch = document.querySelector('.input__search');
 const captions = document.querySelectorAll('figcaption');
+
+// Images
+const a = document.querySelectorAll('a');
+const figcaptions = document.querySelectorAll('figcaption');
+const title = figcaptions[0].textContent;
+
+// Loop through all figures
+const figures = document.querySelectorAll('figure');
 
 // Focus input field on page enter
 inputSearch.focus();
 
 // Filter images
 inputSearch.addEventListener('input', (e) => {
+
   for (let i = 0; i < captions.length; i += 1) {
     let figure = captions[i].parentElement;
 
@@ -33,16 +28,7 @@ inputSearch.addEventListener('input', (e) => {
 
 });
 
-// Images
-const a = document.querySelectorAll('a');
-const figcaptions = document.querySelectorAll('figcaption');
-const title = figcaptions[0].textContent;
-
 //  This adds the text in the <figcaption> elements to the jQuery fancybx dataset
-
-// Loop through all figures
-const figures = document.querySelectorAll('figure');
-
 for (let i = 0; i < figures.length; i += 1) {
   const element = figures[i];
   const dataAttribute = element.firstElementChild.dataset;
@@ -51,3 +37,15 @@ for (let i = 0; i < figures.length; i += 1) {
   // Dynamically add data-caption attributes containing <figcaption> text
   dataAttribute.caption = figcaption.textContent;
 }
+
+
+
+
+// TODO: Remove test code after optomization/refactoring
+// console.log(captions)
+
+// captions.forEach((element, x, y) => {
+//   console.log(element)
+//   console.log('x: ', x)
+//   console.log('y: ', y)
+// });
